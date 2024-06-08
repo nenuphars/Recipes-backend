55d0cd5f5d998370f53f74ccf99c101ea3c95cb6
Nadine Rangosch <nrangosch@gmail.com>
2024-06-08 14:12:05 +0200

    updated model and tested create recipe route

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,77 +1,95 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const ingredientSchema = new mongoose.Schema(
+  {
+    ingredient_name: {
+      type: String,
+      required: true,
+    },
+    ingredient_amount: {
+      type: Number,
+      required: true,
+    },
+    ingredient_measuring: {
+      type: String,
+      enum: ["g", "kg", "ml", "l", "pinch", "whole", "tbsp", "tsp", "cups", "bunch"],
+      required: true,
+    },
+  },
+  { _id: false }
+);
 
-const ingredientSchema = new mongoose.Schema({
-  ingredient_name: {
+const commentSchema = new mongoose.Schema(
+  {
     type: String,
-    required: true
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    date_published: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  ingredient_amount: {
-    type: Number,
-    required: true
-  },
-  ingredient_measuring: {
-    type: String,
-    enum: [
-      "g",
-      "kg",
-      "ml",
-      "l",
-      "pinch",
-      "whole",
-      "half",
-      "tablespoons",
-      "teaspoons"
-    ],
-    required: true
-  }
-}, { _id: false });
-
-const commentSchema = new mongoose.Schema({
-  type: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  },
-  date_published: {
-    type: Date,
-    default: Date.now
-  }
-}, { _id: false });
+  { _id: false }
+);
 
 const recipeSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   photo_url: {
-    type: String
+    type: String,
   },
   duration: {
-    type: Number
+    type: Number,
   },
   ingredientsList: [ingredientSchema],
-  preparation_method: {
+  preparation: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   servings: {
     type: Number,
-    required: true
+    required: true,
   },
   tags: {
-    type: String,
-    enum: ["Italian 🌎", "Pasta 🍝", "Comfort food 🛏️"]
+    type: [String],
+    enum: [
+      "Pasta 🍝",
+      "Comfort food 🛏️",
+      "Chicken 🍗",
+      "Salad 🥗",
+      "Vegetarian 🥣",
+      "Tacos 🌮",
+      "Beef 🥩",
+      "Curry 🍛",
+      "Seafood 🦞",
+      "Grilled ♨️",
+      "Healthy ❤️",
+      "Rice 🍚",
+      "Stew 🍲",
+      "Soup 🍜",
+      "Vegan 🥦",
+      "Quick & Easy ⚡",
+      "Fish 🐟",
+      "Pork 🐖",
+      "Sandwiches 🥪",
+      "Fruity 🍋",
+      "Spicy 🌶️"
+      
+    ],
   },
   creator: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
-  comments: [commentSchema]
+  comments: [commentSchema],
 });
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
